227c49590a720c3fa03922e48e298ad3caa0fe6b
Katie Macaulay <katiemacaulay@Katies-MacBook-Pro.local>
2019-04-22 13:56:17 -0500

    added static

--- teacherscheduler/server/index.js
+++ teacherscheduler/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
+const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb://birdman:m3owm3ow@ds163530.mlab.com:63530/final-project', {useNewUrlParser: true});
@@ -10,6 +11,11 @@ const userRoutes = require("./routes/userRoute")
 app.use(bodyParser.json());
 app.use(userRoutes);
 
+app.use(express.static(path.resolve( __dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'.client/build/index.html'));
+});
 
 app.listen(process.env.PORT ||
   5000, (err) => {
